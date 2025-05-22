3a3078e6e1ca5b37b582ed37ed83ea9b0ebd9ee2
Harsh Patel <patelharsh749005@gmail.com>
2025-05-22 17:44:56 +0530

    List

---
+++ List.dart
@@ -0,0 +1,10 @@
+void main() {
+  List ? fruits = List.filled(5,null , growable:false);
+  fruits[0] = "Keda";
+  fruits[1] = "Keri";
+  fruits[2] = "Dhrax";
+  fruits[3] = "Safarjan";
+  fruits[4] = "Chhiku";
+  print(fruits);
+}
+
